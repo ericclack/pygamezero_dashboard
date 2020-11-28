acc80a42cf063951d922ecdf620c14d166efce91
Eric Clack <ericclack@Erics-Mac.fritz.box>
2020-11-28 16:44:25 +0000

    More examples

--- warhammer.py
+++ warhammer.py
@@ -6,12 +6,12 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # Get the page
-page = requests.get('https://www.bbc.co.uk/weather/2654710/today')
-
+url = 'https://www.warhammer-community.com/2020/11/25/the-flaw-made-manifest-the-death-company-on-the-battlefield/'
+page = requests.get(url)
+print(page.content)
 tree = html.fromstring(page.content)
 
 # The first H1 is the area
-area = tree.xpath('//h1/text()')[0]
-print("Weather forecast for " + area + "\n")
-
+titles = tree.xpath('//h1/text()')
+print(titles)
 
